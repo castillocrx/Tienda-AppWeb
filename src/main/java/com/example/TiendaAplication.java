e410c9fd11376b18048f66e6f8d7cef1531d0621
castillocrx <105761899+castillocrx@users.noreply.github.com>
2023-06-03 20:28:36 -0600

    Ejemplos incluidos con cambios

---
+++ src/main/java/com/example/TiendaAplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaAplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaAplication.class, args);
+	}
+
+}

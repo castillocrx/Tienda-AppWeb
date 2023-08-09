090c7b64dd3019c364ebed90d0d6f46390e8babc
castillocrx <105761899+castillocrx@users.noreply.github.com>
2023-08-08 18:24:38 -0600

    Cambios semana 11-12

--- src/main/java/com/tienda/dao/UsuarioDao.java
+++ src/main/java/com/tienda/dao/UsuarioDao.java
@@ -8,7 +8,11 @@ import com.tienda.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioDao extends JpaRepository<Usuario, Long> {
-
     Usuario findByUsername(String username);
+    
+    Usuario findByUsernameAndPassword(String username, String Password);
 
+    Usuario findByUsernameOrCorreo(String username, String correo);
+
+    boolean existsByUsernameOrCorreo(String username, String correo);
 }
